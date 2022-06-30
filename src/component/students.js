69ee2a9c87271fb5a8c88fcdbc29fee8379b957e
Paignton Wild <paignton.wild@edu.sait.ca>
2022-06-30 11:32:58 -0600

    Initial

---
+++ src/component/students.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const cardInfo = [
+  { image: "s1.png", name: "Mike", gpa: 3.8 },
+  { image: "s2.png", name: "Manal", gpa: 3.6 },
+  { image: "s3.png", name: "Moe", gpa: 3.2 },
+  { image: "s4.png", name: "Mary", gpa: 2.6 },
+];
+
+function students() {
+  return <div>Student here! asnd they are angry</div>;
+}
+
+export default students;
