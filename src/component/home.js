ffe35be3548480e99b2f3fbb2d333082229ff34f
Paignton Wild <paignton.wild@edu.sait.ca>
2022-06-30 11:35:04 -0600

    Updates home to add more information on what Geeks like

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-  return <h1>Welcome to the world of Geeks!</h1>;
+  return (
+    <h1>
+      Welcome to the world of Geeks! We love everything Science and Technology!
+    </h1>
+  );
 }
 
 export default Home;
