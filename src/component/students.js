faac9fa7a73954c16f3e793e90c1cfd4d1455f3c
Paignton Wild <paignton.wild@edu.sait.ca>
2022-06-30 22:13:35 -0600

    Adds Student 1 image

--- src/component/students.js
+++ src/component/students.js
@@ -10,7 +10,31 @@ const cardInfo = [
 ];
 
 function students() {
-  return <div>Student here!</div>;
+  return (
+    <div>
+      Student here!
+      <div className="grid-container">
+        <div class="grid-item">
+          <img src={require("./s1.png")}></img>
+          <h1></h1>
+        </div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+      </div>
+      {/* card style that throws hook errors: <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card> */}
+    </div>
+  );
 }
 
 export default students;
